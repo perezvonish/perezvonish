b23af3feea007e297a17cab43448427e39d592fe
Vladimir <shokilak@gmail.com>
2023-06-14 21:05:54 +0600

    feat: main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Info} from "./components/Info";
+import {Navigation} from "./components/Navigation";
+import {About} from "./components/About";
+import {Projects} from "./components/Projects";
+import {Contacts} from "./components/Contacts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Info />
+          <About />
+          <Projects />
+          <Contacts />
+        <Navigation />
+      </div>
   );
 }
 
---
+++ src/components/Info.tsx
@@ -0,0 +1,11 @@
+export const Info = () => {
+    return (
+        <div>
+            let developer = {"{"}
+                name: Vladimir Korobenko,
+                age: 20,
+                role: "middle node.js developer"
+            {"}"}
+        </div>
+    )
+}

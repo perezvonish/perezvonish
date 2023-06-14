412c133051f7dc9a670f5b52eb14961726504081
Vladimir <shokilak@gmail.com>
2023-06-15 01:44:34 +0600

    feat: panel backgound

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,39 +1,35 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 export const Navigation = () => {
     const [activeComponent, setActiveComponent] = useState('Info');
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const scrollPosition = window.scrollY;
-            const components = document.getElementsByClassName('component');
-
-            // Определение активного компонента на основе положения скролла
-            for (let i = 0; i < components.length; i++) {
-                const component = components[i] as HTMLElement;
-                const offsetTop = component.offsetTop;
-                const componentHeight = component.offsetHeight;
-
-                if (scrollPosition >= offsetTop && scrollPosition < offsetTop + componentHeight) {
-                    setActiveComponent(component.id);
-                    break;
-                }
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
     return (
         <div className="status-panel">
             <ul className="component-list">
-                <li className={activeComponent === 'Info' ? 'active' : ''}>Info</li>
-                <li className={activeComponent === 'About' ? 'active' : ''}>About</li>
-                <li className={activeComponent === 'Contacts' ? 'active' : ''}>Contacts</li>
-                <li className={activeComponent === 'Navigation' ? 'active' : ''}>Navigation</li>
+                <li
+                    className={activeComponent === 'Info' ? 'active' : ''}
+                    onClick={() => setActiveComponent('Info')}
+                >
+                    {"<"}Info{">"}
+                </li>
+                <li
+                    className={activeComponent === 'About' ? 'active' : ''}
+                    onClick={() => setActiveComponent('About')}
+                >
+                    {"<"}About{">"}
+                </li>
+                <li
+                    className={activeComponent === 'Contacts' ? 'active' : ''}
+                    onClick={() => setActiveComponent('Contacts')}
+                >
+                    {"<"}Contacts{">"}
+                </li>
+                <li
+                    className={activeComponent === 'Navigation' ? 'active' : ''}
+                    onClick={() => setActiveComponent('Navigation')}
+                >
+                    {"<"}Navigation{">"}
+                </li>
             </ul>
         </div>
     );
